50752ec9235a95b5038c43ba0c6e18c9b3ab0e21
Justin Dodson <justin.dodson@agoradata.com>
2024-05-01 11:09:57 -0500

    session 1: got a simple http server running in our python module

---
+++ README.md
@@ -0,0 +1,7 @@
+## PYTALK
+
+#### Running the project:
+1. cd into the pytalk directory
+2. run `python3 -m venv venv` to create a new virtual environment. *Note: you may need to run `pip3 install venv` in the module is not already installed.*
+3. run `source venv/bin/activate` to initialize the environment. 
+4. run `python main.py` to start the server.
--- pytalk/main.py
+++ pytalk/main.py
@@ -1,6 +1,14 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
 
 def main():
-    print("Hello world!")
+    run()
 
 
 if __name__ == "__main__":
