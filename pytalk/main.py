a2a142cc9a24f9e8a1bc7725a3aac481fe4c9ba5
Justin Dodson <justin.dodson@agoradata.com>
2024-05-09 15:07:43 -0500

    get the echo server working

--- pytalk/main.py
+++ pytalk/main.py
@@ -1,7 +1,32 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+class PytalkHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        match self.path:
+            case "/":
+                print("in base path")
+                return
+            case "/echo":
+                print("in echo")
+                self.echoHandler()
+                return
+            case _:
+                self.send_error(404, message='nah bitch...')
+                return
+
+    def echoHandler(self):
+        content_length = int(self.headers.get("Content-Length"))
+        body = self.rfile.read(content_length-1)
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", len(body))
+        self.end_headers()
+        self.wfile.write(body)
+
+
+def run(server_class=HTTPServer, handler_class=PytalkHandler):
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
