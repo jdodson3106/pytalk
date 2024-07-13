0b08efb2f10dd7135094b5c4d8b45fc7853ccaa6
william-h-jones <william.jones@agoradata.com>
2024-07-12 19:09:31 -0500

    Issue #4
    Overwrote the do_GET, do_POST methods (provided by pythons http.server BaseHTTPRequestHandler library) to do some basic request handling
    Note that the BaseHTTPRequestHandler library that's being leveraged here handles a lot of the parsing for us. The only thing we need to need to perform custom parsing on is the body attached to the POST request. Body can currently only support form-data, but has no way of distinguishing form-data from non-form-data.
    
    Note that while I was finishing this up, I noticed that the headers object isn't a dictionary but a HTTPMessage object. This is fine for now, but will need to get looked at later

--- pytalk/server/request_handler.py
+++ pytalk/server/request_handler.py
@@ -1,10 +1,39 @@
 from http.server import BaseHTTPRequestHandler
-
+from request.request import Request
 
 class PytalkRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        pass
+
+        request = Request(
+            method = self.command, 
+            path = self.path,
+            headers = self.headers
+        )
+        return request 
 
     def do_POST(self):
-        pass
+        
+        # Parse body into key-value pairs
+        # Currently only accepts form data
+        content_length = int(self.headers.get("Content-Length"))
+        body = self.rfile.read(content_length).decode("utf-8")
+        body_lines = body.split("\r\n")
+        
+        form_data = {}
+        for i, body_line in enumerate(body_lines): 
+            if not body_line:
+                continue
+            if "name=" in body_line:
+                key = body_line.split("name=")[-1][1:-1] #strip quotes
+                value = body_lines[i+2]
+                form_data[key] = value
+        
+        request = Request(
+            method = self.command, 
+            path = self.path,
+            form_data = form_data,
+            headers = self.headers
+        )
+        print(request.headers)
+        return request
