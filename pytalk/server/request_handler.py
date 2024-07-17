ceaa5660fdb49cd4bd02aeaa49f877070e6e276e
william-h-jones <93158278+william-h-jones@users.noreply.github.com>
2024-07-16 19:35:58 -0500

    Update request_handler.py
    Remove responsibility of parsing different body from the do_POST method. Since different kinds of "body"s can come into the Request class, my thinking is that the body should enter into the Request class in it's rawest possible form where it is then parsed.

--- pytalk/server/request_handler.py
+++ pytalk/server/request_handler.py
@@ -18,24 +18,15 @@ class PytalkRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         
         # Parse body into key-value pairs
-        # Currently only accepts form data
+        # Currently only accepts form data, html
         content_length = int(self.headers.get("Content-Length"))
+        content_type = self.headers.get("Content-Type")
         body = self.rfile.read(content_length).decode("utf-8")
-        body_lines = body.split("\r\n")
-        
-        form_data = {}
-        for i, body_line in enumerate(body_lines): 
-            if not body_line:
-                continue
-            if "name=" in body_line:
-                key = body_line.split("name=")[-1][1:-1] #strip quotes
-                value = body_lines[i+2]
-                form_data[key] = value
         
         request = Request(
             method = self.command, 
             path = self.path,
-            form_data = form_data,
+            body = body,
             headers = self.headers
         )
         logger.info(request)
