13e005026bf447584cce388acca85bc442023eb6
Justin Dodson <justin.dodson@agoradata.com>
2024-08-02 16:21:55 -0500

    ordering matters...

--- pytalk/server/request_handler.py
+++ pytalk/server/request_handler.py
@@ -1,27 +1,25 @@
-from http.server import BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
 from http import HTTPStatus
 from pytalk.request.request import Request
 from pytalk.logger.logger import get_logger
-import sys
 import os
-import shutil
 
 logger = get_logger()
+
 
 class PytalkRequestHandler(BaseHTTPRequestHandler):
 
     def log_message(self, format, *args):
-        logger.info("%s - %s" %(self.address_string(),format%args))
-
+        logger.info("%s - %s" % (self.address_string(), format % args))
 
     def do_GET(self):
 
         request = Request(
-            method = self.command, 
-            path = self.path,
-            headers = self.headers
+            method=self.command,
+            path=self.path,
+            headers=self.headers
         )
-        return request 
+        return request
 
     def do_POST(self):
         
@@ -31,10 +29,10 @@ class PytalkRequestHandler(BaseHTTPRequestHandler):
         body = self.rfile.read(content_length).decode("utf-8")
         
         request = Request(
-            method = self.command, 
-            path = self.path,
-            body = body,
-            headers = self.headers
+            method=self.command,
+            path=self.path,
+            body=body,
+            headers=self.headers
         )
 
         # So I can send an error back to the sender
@@ -43,23 +41,14 @@ class PytalkRequestHandler(BaseHTTPRequestHandler):
         # But I can't send a valid response? Trying to echo request
         """Serve a GET request."""
         
-        with open("pytalk\server\dummy.html", "rb") as fin: 
-            
-            shutil.copyfileobj(fin, self.wfile) # copy fin -> self.wfile
-            print(self.wfile.fileno(), fin.fileno())
+        with open("./pytalk/server/dummy.html", "rb") as fin:
             fs = os.fstat(fin.fileno())
-            #_ = self.wfile.readable()
-            #logger.info(_)
-            logger.info("")
             logger.info(fs)
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-Type", "text/html")
+            self.send_header("Content-Length", str(fs.st_size))
+            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+            self.end_headers()
 
-
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(fs.st_size))
-        #self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-        self.end_headers()
-
-        #self.send_response(HTTPStatus.OK)
-        #self.end_headers()
-        #return request
+            to_write = fin.read()
+            self.wfile.write(to_write)
